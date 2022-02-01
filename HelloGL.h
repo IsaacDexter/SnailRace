3a4a1dbc04e76025e2a8ead469adc027d6fb1f21
Isaac Dexter <d015349l@student.staffs.ac.uk>
2022-02-01 14:36:18 +0000

    included required files for freeGLUT and OpenGL

--- HelloGL.h
+++ HelloGL.h
@@ -1,4 +1,9 @@
 #pragma once
+#include <Windows.h>					//Required for OpenGl on Windows
+#include <gl/GL.h>						//OpenGL
+#include <gl/GLU.h>						//OpenGL Utilities
+#include "freeglut.3.2.1/GL/freeglut.h"	//freeGLUT library
+
 class HelloGL
 {
 };
--- main.cpp
+++ main.cpp
@@ -6,3 +6,5 @@ int main(int argc, char* argv[])
 
 	return 0;
 }
+
+//Task 4: Implementing a simple OpenGL scene
