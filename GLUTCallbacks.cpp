5d37212409470f32e3e5ce7840daeee1789ee014
Isaac Dexter <d015349l@student.staffs.ac.uk>
2022-02-08 13:57:19 +0000

    The program now updates indefinitely ever 16ms (at ~60fps). However this is inperceptable as nothing changes.

--- GLUTCallbacks.cpp
+++ GLUTCallbacks.cpp
@@ -27,5 +27,7 @@ namespace GLUTCallbacks
 	{
 		//Calls the update function
 		helloGL->Update();
+		//Sets a new timer for the same FPS. not hardcoded in case it changes during the program. Calls itself in turn, effectively updating the program every 16ms for perpetua.
+		glutTimerFunc(preferredRefresh, GLUTCallbacks::Timer, preferredRefresh);
 	}
 }
