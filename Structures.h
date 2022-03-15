47f0d4ab371ebe39c6884538b269023daef4240a
Isaac Dexter <d015349l@student.staffs.ac.uk>
2022-03-15 14:12:43 +0000

    Added new cube.txt

--- Structures.h
+++ Structures.h
@@ -81,12 +81,6 @@ struct Camera
 	Vector3 up;
 };
 
-/// <summary>Used to store colours for each vertex in a 3D shape. uses GLfloat to avoid compiler warnings</summary>
-struct Color
-{
-	GLfloat r, g, b;
-};
-
 /// <summary>Used to store vertexes in a 3D shape. uses GLfloat to avoid compiler warnings</summary>
 struct Vertex
 {
@@ -106,12 +100,12 @@ struct Mesh
 {
 	///<summary>All indexed vertices in the shape</summary>
 	Vertex* Vertices;
+	/// <summary>The coordinates of the texture to be loaded onto the mesh</summary>
+	TexCoord* TexCoords;
 	/// <summary>The Normals, which are used in giving a shape the illusion of texture. Can either be calculated automatically or loaded from a file.</summary>
 	Vector3* Normals;
 	/// <summary>Defines the triangles that make up the shape, using the indices of each of the vertices/colors.  GLuShort is an in built typedef of 16 bit unsigned binary integer.</summary>
 	GLushort* Indices;
-	/// <summary>The coordinates of the texture to be loaded onto the mesh</summary>
-	TexCoord* TexCoords;
 	//ints used in the loading of files.
 	int VertexCount, ColorCount, IndexCount, TexCoordCount;
 };
